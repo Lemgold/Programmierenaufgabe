38f684938c4659838abcc46c84a87d3fabba0b1d
s9ADROID <s9adroid@ba-dresden.de>
2017-11-07 09:15:38 +0100

    Kommentare ergänzt

--- app/src/main/java/herdenmanagement/model/Gras.java
+++ app/src/main/java/herdenmanagement/model/Gras.java
@@ -5,6 +5,16 @@ package herdenmanagement.model;
  */
 public class Gras extends PositionsElement {
 
+    /**
+     * Er sprach "Wachse und gedeihe" und es wuchs und gedieh. Die Methode
+     * verknüpft das Gras mit dem Acker (siehe {@link Acker#graeser}) und setzt
+     * die Position für das Gras. Eine Prüfung der Position erfolgt nicht.
+     * Theoretisch kann man also auch Gras außerhalb des Ackers platzieren.
+     * Aber warum?
+     *
+     * @param acker Acker, auf dem das Gras wachsen soll
+     * @param position Position auf dem Acker für das Gras
+     */
     public void wachseUndGedeihe(Acker acker, Position position) {
         setzeAcker(acker);
         setzePosition(position);

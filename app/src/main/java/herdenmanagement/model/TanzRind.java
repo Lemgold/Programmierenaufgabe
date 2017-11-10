2cbca2f1e4ef1fda4c52ec99e9d5ff0a14af75df
greiffenberg <steffen.greiffenberg@ba-dresden.de>
2017-11-10 15:41:21 +0100

    Skelett wiederhergestellt

--- app/src/main/java/herdenmanagement/model/TanzRind.java
+++ app/src/main/java/herdenmanagement/model/TanzRind.java
@@ -1,9 +1,5 @@
 package herdenmanagement.model;
 
-import android.media.MediaPlayer;
-
-import de.ba.herdenmanagement.R;
-
 /**
  * Tanzrinder erben alle Eigenschaften der Klasse {@link Rindvieh}. Sie können sich also
  * genauso auf einem Acker bewegen. Tanzrinder können zusätzlich seitwärts gehen.
@@ -31,22 +27,7 @@ public class TanzRind extends Rindvieh {
     protected Position gibNaechstePositionRechts() {
         Position position = gibPosition();
 
-        if (gibRichtung() == RichtungsTyp.NORD) {
-            position.x = position.x + 1;
-            return position;
-        } else if (gibRichtung() == RichtungsTyp.OST) {
-            position.y = position.y - 1;
-            return position;
-        } else if (gibRichtung() == RichtungsTyp.SUED) {
-            position.x = position.x - 1;
-            return position;
-        } else if (gibRichtung() == RichtungsTyp.WEST) {
-            position.y = position.y + 1;
-            return position;
-        }
 
-        // Da die Kuh stets in eine der obigen Richtungen schauen sollte,
-        // kommen wir hier nur dann an, wenn die Richtung der Kuh nicht definiert ist.
         return position;
     }
 
@@ -56,22 +37,6 @@ public class TanzRind extends Rindvieh {
     protected Position gibNaechstePositionLinks() {
         Position position = gibPosition();
 
-        if (gibRichtung() == RichtungsTyp.NORD) {
-            position.x = position.x - 1;
-            return position;
-        } else if (gibRichtung() == RichtungsTyp.OST) {
-            position.y = position.y + 1;
-            return position;
-        } else if (gibRichtung() == RichtungsTyp.SUED) {
-            position.x = position.x + 1;
-            return position;
-        } else if (gibRichtung() == RichtungsTyp.WEST) {
-            position.y = position.y - 1;
-            return position;
-        }
-
-        // Da die Kuh stets in eine der obigen Richtungen schauen sollte,
-        // kommen wir hier nur dann an, wenn die Richtung der Kh nicht definiert ist.
         return position;
     }
 
@@ -79,25 +44,14 @@ public class TanzRind extends Rindvieh {
      * Bewegt das Rind seitwärts nach links
      */
     public void geheSeitwaertsNachLinks() {
-        Position naechstePosition = gibNaechstePositionLinks();
-        if (gehtsDaLinksWeiter()) {
-            setzePosition(naechstePosition);
-        } else {
-            setzeNachricht(R.string.rindvieh_nix_acker);
-        }
+
     }
 
     /**
      * Bewegt das Rind seitwärts nach rechts
      */
     public void geheSeitwaertsNachRechts() {
-        Position naechstePosition = gibNaechstePositionRechts();
-        Acker acker = gibAcker();
-        if (acker.istPositionGueltig(naechstePosition)) {
-            setzePosition(naechstePosition);
-        } else {
-            setzeNachricht(R.string.rindvieh_nix_acker);
-        }
+
     }
 
     /**
@@ -106,9 +60,7 @@ public class TanzRind extends Rindvieh {
      * @return true, wenn die Kuh auf dem Acker weiter nach lnks gehen kann
      */
     public boolean gehtsDaLinksWeiter() {
-        Position naechstePosition = gibNaechstePositionLinks();
-        Acker acker = gibAcker();
-        return acker.istPositionGueltig(naechstePosition);
+        return false;
     }
 
     /**
@@ -117,23 +69,13 @@ public class TanzRind extends Rindvieh {
      * @return true, wenn die Kuh auf dem Acker weiter nach links gehen kann
      */
     public boolean gehtsDaRechtsWeiter() {
-        Position naechstePosition = gibNaechstePositionRechts();
-        Acker acker = gibAcker();
-        return acker.istPositionGueltig(naechstePosition);
+        return false;
     }
 
-    public void chaChaCha(MediaPlayer player) {
-        player.start();
+    /**
+     * Das TanzRind tanzt Cha Cha Cha!
+     */
+    public void chaChaCha() {
 
-        for (int i = 0; i < 10; i++) {
-            geheVor();
-            geheZurueck();
-            geheSeitwaertsNachLinks();
-            geheSeitwaertsNachLinks();
-            geheZurueck();
-            geheVor();
-            geheSeitwaertsNachRechts();
-            geheSeitwaertsNachRechts();
-        }
     }
 }
